9b214dad21fb046210f480f4272c9f6f27f7d97c
laughatpain <laughatpain@users.noreply.github.com>
2021-05-06 12:25:03 -0600

    Fixing Stuff

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+
+/* Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
  */
 
@@ -10,12 +10,11 @@ import "../../assets/icon-80.png";
 
 /* global document, Office */
 
-const ssoAuthHelper = require("./../helpers/ssoauthhelper");
+// const ssoAuthHelper = require("./../helpers/ssoauthhelper");
 
 Office.onReady(info => {
   if (info.host === Office.HostType.Outlook) {
     document.getElementById("addSignatures").onclick = addSign;
-     console.log ("added new code");
     // Create a "close" button and append it to each list item
     var myNodelist = document.getElementsByTagName("LI");
     var i;
